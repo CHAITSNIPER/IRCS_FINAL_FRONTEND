d0a3547465c18235ee040da966eeab22fbe4d496
CHAITSNIPER <email>
2024-06-20 23:44:18 +0530

    route change

--- src/utils/API-routes.js
+++ src/utils/API-routes.js
@@ -1,4 +1,4 @@
-export const host = `http://127.0.0.1:4000`;
+export const host = `https://ircsbackend.onrender.com`;
 export const postProjects = `${host}/authenticateRoute/postProjects`;
 export const getAllProjects = `${host}/projects/getAllProjects`;
 export const getSelectedProject = (id) =>`${host}/projects/getSelectedProjects/${id}`;
