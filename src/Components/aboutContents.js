0b9c940013937021eb9434bd9256407badd9e244
Utkarsh Sharma <utkarshsharma@Utkarshs-MacBook-Air-2.local>
2024-06-28 17:11:16 +0530

    cors changed

--- src/Components/aboutContents.js
+++ src/Components/aboutContents.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 export const HistorySection = () => (
     <section>
       <h1 className="h1About">Our History</h1>
-      <p>Content about the organization's history...</p>
+      <h2>HISTORY OF RED CROSS AND RED CRESCENT MOVEMENT</h2>
+      <p>Young Swiss businessman, Jean Henry Dunant was appalled by the condition of the wounded soldiers he happened to see in the battle field of Solferino, Italy in 1859 during the Franco – Austrian war. He arranged relief services with the help of the local community immediately. He wrote the book ‘Memory of Solferino’ suggesting that a neutral organization be established to aid the wounded soldiers in times of war. Just a year after the release of this book, an international conference was convened in Geneva to consider the suggestions of Henry Dunant and thus the Red Cross Movement was born. International Red Cross Movement was established by Geneva Convention of 1864. The name and the emblem of the movement are derived from the reversal of the Swiss national flag, to honor the country in which Red Cross was found.</p>
     </section>
   );
   
@@ -18,12 +19,12 @@ export const HistorySection = () => (
       <h2>THE MISSION OF THE INDIAN RED CROSS SOCIETY</h2>
       <p>Indian Red Cross Society is constituted by the Government of India by Act XV of 1920 passed by the Indian Parliament. It is the instrument chosen by Indian Legislature to help carry out the obligations assumed by India under international treaties known as Geneva Convention. The legislation imposes on Red Cross the duties to act for the voluntary relief to the Indian people and the Armed Forces, and to take suitable measures to ensure national and international relief to prevent and mitigate sufferings caused by disasters. All the activities of Indian Red Cross Society support these duties. Nationally and locally Indian Red Cross is governed by volunteers, most of its duties are performed by volunteers, and it is financed mostly by voluntary contributions.</p>
       <h2>FUNCTIONS OF NATIONAL RED CROSS SOCIETY</h2>
-      <p>Relief (National and International)
-        Development of Volunteer base.
-        Health, Social Welfare, Nursing, First Aid, Maternity and Child Welfare.
-        Help to disabled persons, weaker sections of society.
-        Blood Donation.
-        Junior and Youth Red Cross.
+      <p>Relief (National and International)<br></br>
+        Development of Volunteer base.<br></br>
+        Health, Social Welfare, Nursing, First Aid, Maternity and Child Welfare.<br></br>
+        Help to disabled persons, weaker sections of society.<br></br>
+        Blood Donation.<br></br>
+        Junior and Youth Red Cross.<br></br>
         Information and Dissemination.</p>
       <h2>ACTIVITIES OF INDIAN RED CROSS SOCIETY (IRCS):</h2>
       <p>The Indian Red Cross Society helped soldiers and war prisoners during crisis with neighbouring countries. IRCS is always in the frontline to help people during natural calamities in situation such as cyclones, earthquakes, floods, famines. The general activities of Indian Red Cross are in the field of family welfare, antenatal and postnatal care, immunisation, eradication of TB, polio and other like diseases. About four lakh units of blood are collected every year through Red Cross blood banks. Artificial limbs and other accessories such as wheel chairs and hearing aids are provided to needy and poor people.</p>
@@ -39,7 +40,21 @@ export const HistorySection = () => (
  export const MumbaiSection = () => (
     <section>
         <h1 className="h1About">IRCS Mumbai</h1>
-        <p>mumbai the city of dreams...</p>
+        <h2>INDIAN RED CROSS SOCIETY, MAHARASHTRA STATE BRANCH</h2>
+        <p>The Maharashtra branch was founded in 1921. The Maharashtra State Red Cross is active in 29 districts. Honorable Minister of Health is the Chairman of the State Branch. Honorable Governor of Maharashtra is the President of the State Branch. The State Branch has played a marvelous historic role in all calamities such as Koyana earthquake in sixties, devastating famines in seventies, the bomb blasts and riots in Mumbai in nineties, the earth quakes in Latur and Osmanabad districts, Gujarat earthquake in January 2001, Tsunami in December 2004, Kosi River floods in Bihar in 2008, floods in Maharashtra in and terrorist attack on Taj Hotel and many other areas in Mumbai in November,2009 and many other state calamities.</p>
+        <h3>ACTIVITIES ON STATE-LEVEL</h3>
+        <p>About 1, 00,000 units of Blood collected through 11 Blood Banks every year and Blood components provided to needy patients without any profit.<br></br>
+        Tuberculosis Sanatorium and special wards of HIV are run at Bel Air hospital (Panchagani).<br></br>
+        Bel Air College of Nursing at Panchagani.<br></br>
+        Workshops on Public Health in Emergencies.<br></br>
+        Disaster Management Training organized in District branches.<br></br>
+        Workshops on vulnerability and capacity assessment and Risk Reduction Programmes..<br></br>
+        State level pre-monsoon review.<br></br>
+        Mobile Health Unit in naxalite affected areas in Gadchiroli. Distribution of agricultural implements and seeds in Gadchiroli District.<br></br>
+        Emblem awareness campaigns.<br></br>
+        Tracing Activities and Restoring family links.<br></br>
+        Youth Peer Education program.<br></br>
+        Humanitarian Pandemic Preparedness Project for awareness on H1N1.</p>
     </section>
  )
 
@@ -52,8 +67,14 @@ export const HistorySection = () => (
  
  export const EmblemSection = ()=> (
     <section>
-        <h1 className="h1About">EMBLEM</h1>
-        <p>...</p>
+        <h1 className="h1About">RED CROSS EMBLEM</h1>
+        <p>Red Cross on a white background, is the Emblem of Red Cross, recognized in 1864 as the distinctive sign for medical relief teams on the battle field.<br></br><br></br>
+
+In the Russo-Turkish war the Ottoman empire used a Red Crescent in place of the Red Cross. Egypt too opted for the Red Crescent while Persia chose a Red Lion on a white background. These symbols were written and accepted into the 1929 Geneva Conventions. The IRCS adopted RED CROSS as its emblem.<br></br><br></br>
+
+The National Society make use of the emblem as an indicative device in peacetime and during armed conflicts within the limits stipulated in national legislation, the regulations and its statutes only for activities consistent with the principles set out by International Federation of the Red Cross and Red Crescent.<br></br><br></br>
+
+During the General Assembly and the council of Delegates in November 2005 at Geneva, Red Crystal has been adopted as another emblem for the Red Cross Red Crescent movement.</p>
     </section>
  )
 
@@ -66,10 +87,12 @@ export const HistorySection = () => (
 
  export const UpSection = () => (
     <section>
-        <h1 className="h1About">Tie Ups</h1>
-        <p>....</p>
+        <h1 className="h1About">We are non-profit organizations. Our charity activities take place all around Mumbai.
+        </h1>
+        <p>We Help Lots Of People By Partnering with them</p>
     </section>
  )
+ 
 
   
           
