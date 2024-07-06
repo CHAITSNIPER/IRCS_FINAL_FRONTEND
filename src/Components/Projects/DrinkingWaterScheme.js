b2d8cb63d86f3aac1c7ce84cbc92bac90402b7c8
CHAITSNIPER <email>
2024-07-06 13:15:30 +0530

    projects added

--- src/Components/Projects/DrinkingWaterScheme.js
+++ src/Components/Projects/DrinkingWaterScheme.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import drws from './images/drws.png';
+import './project.css';
 
 export default function DrinkingWaterScheme() {
   return (
-    <div>
-      <h1>Drinking Water Supply Scheme</h1>
+    <div className="container">
+      <div className="card">
+        <img src= {drws} className='imageInProj'/>
+        <h1 className="header">A TRIBAL VILLAGE IN PANVEL</h1>
+        <p className="para">
+          A greater section of our Indian population is still deprived of basic amenities which is the fundamental right of every Indian human being. It is shocking to know even today that 800 million Indians do not have access to clean drinking water. Despite Government plans and efforts in this direction, the current scenario is very dismal.
+        </p>
+        <p className="para">
+          Thus, it is the objective of NGOs like us to provide drinking water in the villages. The Balwantrai Centre selected those villages which had a population less than 3000, where government help for drinking water was not available. Women have to walk several miles daily to collect water. Agriculture is the main occupation. The crops depend entirely on the monsoon.
+        </p>
+        <p className="para">
+          The drinking water project has served as a stepping stone in the overall development process of the village. Before the water supply project, the women folk used to waste 4-5 hours every day in fetching water. Now women will be able to save the time and energy and utilize the same for productive purpose. Farmers will be able to grow the crop throughout the year. Lift irrigation schemes can also be provided, besides, providing skills for organic farming for the benefit of farmers.
+        </p>
+        <h2 className="sub-header">Drinking Water provided in the following villages –</h2>
+        <ul className="village-list">
+          <li>Babdevwadi</li>
+          <li>Pimpalwadi</li>
+          <li>Mahalungi Chindran High School</li>
+          <li>Khairwadi</li>
+          <li>Khanavale</li>
+          <li>Shiravli</li>
+          <li>Shivansai Primary School</li>
+          <li>Chanchavali</li>
+        </ul>
+        <p className="para">
+          Indian Red Cross Society Bombay City Branch has done a wonderful job in providing this basic amenity for the comfort of villagers.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
+
--- src/Components/Projects/KashFlood.js
+++ src/Components/Projects/KashFlood.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import jkfloods from './images/jkfloods.png';
+import './project.css';
 
 export default function KashFlood() {
   return (
-    <div>
-      <h1>JAMMU & KASHMIR FLOODS</h1>
+    <div className="container">
+      <div className="card">
+      <img src={jkfloods} className='imageInProj'/>
+      <h1 className='header'>JAMMU & KASHMIR FLOODS</h1>
+      <p className='para'>In respone to Bombay city Red Cross appeal for victims 
+        of Jammu & Kashmir Rs. 7,71,000/- were collected from which Rs. 4,73,000/- 
+        was spent in procuring the much needed relief supplies requested for by the Kashmir 
+        Red Cross Unit namely 2000 blankets, 50000 face masks ,25000 examination gloves and 174 
+        towels. In addition, educational request was given priority by sending 2500 note books & 
+        education accessories such as rubbers, pencils, slates, slate pencils & scales.</p>
+        </div>
     </div>
   )
 }
